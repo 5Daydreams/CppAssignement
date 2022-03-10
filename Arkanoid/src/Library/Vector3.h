1915c434ff406ad696c7fe55ed263ca5a713bc68
5Daydreams <nelsonkossuga@gmail.com>
2022-03-10 20:18:39 +0100

    broke the vector3 library; working on fixing the methods to work only within the struct scope;

--- Arkanoid/src/Library/Vector3.h
+++ Arkanoid/src/Library/Vector3.h
@@ -7,98 +7,99 @@ struct Vector3
 	float x = 0;
 	float y = 0;
 	float z = 0;
-};
 
-inline Vector3 VectorAbs(Vector3 v)
-{
-	return {
-		std::abs(v.x),
-		std::abs(v.y),
-		std::abs(v.z)
-	};
-}
+	Vector3 VectorAbs(Vector3 v)
+	{
+		return {
+			std::abs(v.x),
+			std::abs(v.y),
+			std::abs(v.z)
+		};
+	}
 
-inline Vector3 operator+(Vector3 left, Vector3 right)
-{
-	return {
+	Vector3 operator+(Vector3 right)
+	{
+		return {
+			this->x + right.x,
+			this->y + right.y,
+			this->z + right.z
+		};
+	}
+
+	Vector3 operator-(Vector3 right)
+	{
+		return {
+			this->x - right.x,
+			this->y - right.y,
+			this->z - right.z
+		};
+	}
+
+	Vector3& operator+=(Vector3 right)
+	{
+		return this = {
 		left.x + right.x,
 		left.y + right.y,
 		left.z + right.z
-	};
-}
+		};
+	}
 
-inline Vector3 operator-(Vector3 left, Vector3 right)
-{
-	return {
-		left.x - right.x,
-		left.y - right.y,
-		left.z - right.z
-	};
-}
+	inline Vector3 operator*(Vector3 vec, float scalar)
+	{
+		return {
+			vec.x *= scalar,
+			vec.y *= scalar,
+			vec.z *= scalar,
+		};
+	}
 
-inline Vector3& operator+=(Vector3& left, Vector3 right)
-{
-	return left = {
-	left.x + right.x,
-	left.y + right.y,
-	left.z + right.z
-	};
-}
+	inline Vector3 operator*(float scalar, Vector3 vec)
+	{
+		return {
+			vec.x *= scalar,
+			vec.y *= scalar,
+			vec.z *= scalar,
+		};
+	}
 
-inline Vector3 operator*(Vector3 vec, float scalar)
-{
-	return {
-		vec.x *= scalar,
-		vec.y *= scalar,
-		vec.z *= scalar,
-	};
-}
+	inline Vector3 operator*(Vector3 vec, int scalar)
+	{
+		return {
+			vec.x *= scalar,
+			vec.y *= scalar,
+			vec.z *= scalar,
+		};
+	}
 
-inline Vector3 operator*(float scalar, Vector3 vec)
-{
-	return {
-		vec.x *= scalar,
-		vec.y *= scalar,
-		vec.z *= scalar,
-	};
-}
+	inline Vector3 operator*(int scalar, Vector3 vec)
+	{
+		return {
+			vec.x *= scalar,
+			vec.y *= scalar,
+			vec.z *= scalar,
+		};
+	}
 
-inline Vector3 operator*(Vector3 vec, int scalar)
-{
-	return {
-		vec.x *= scalar,
-		vec.y *= scalar,
-		vec.z *= scalar,
-	};
-}
+	inline float VectorDot(Vector3 left, Vector3 right)
+	{
+		return (left.x * right.x + left.y * right.y + left.z * right.z);
+	}
 
-inline Vector3 operator*(int scalar, Vector3 vec)
-{
-	return {
-		vec.x *= scalar,
-		vec.y *= scalar,
-		vec.z *= scalar,
-	};
-}
+	inline float VectorLength(Vector3 vec)
+	{
+		return std::sqrt(VectorDot(vec, vec));
+	}
 
-inline float VectorDot(Vector3 left, Vector3 right)
-{
-	return (left.x * right.x + left.y * right.y + left.z * right.z);
-}
+	inline float VectorDistance(Vector3 left, Vector3 right)
+	{
+		Vector3 distVec = left - right;
 
-inline float VectorLength(Vector3 vec)
-{
-	return std::sqrt(VectorDot(vec, vec));
-}
+		return VectorLength(distVec);
+	}
 
-inline float VectorDistance(Vector3 left, Vector3 right)
-{
-	Vector3 distVec = left - right;
+	inline float DotNormalized(Vector3 left, Vector3 right)
+	{
+		return (left.x * right.x + left.y * right.y + left.z * right.z);
+	}
 
-	return VectorLength(distVec);
-}
-
-inline float DotNormalized(Vector3 left, Vector3 right)
-{
-	return (left.x * right.x + left.y * right.y + left.z * right.z);
-}
+};
